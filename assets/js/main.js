8819a31b5ca8a7db528e1b0b622400b583edf897
GerkJa <gerkmanj@arcada.fi>
2023-11-06 23:32:06 +0200

    Cool stuff

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
 /* Site-wide JS i den här filen (t.ex. huvudmenyn) */
 
 console.log('main.js init'); // För att se att skriptet laddats in
+
+//Huvudmeny slide function
+function openNav() {
+    document.getElementById("menu").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("menu").style.width = "0";
+}
+
