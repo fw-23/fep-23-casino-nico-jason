d14348ed6d07e441de48e3ee7f50568b499caf44
nico <janssnic@arcada.fi>
2023-11-21 21:55:47 +0200

    städade koden

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,6 +13,7 @@ function closeNav() {
 let username;
 let credits;
 let age;
+let aged = +age
 let playtime;
 let playTimeStop;
 //date and time
@@ -23,97 +24,88 @@ function InfoDisplay() {
     let h = date.getHours();
     let m = date.getMinutes();
     let s = date.getSeconds();
-    
-    
+
+
     let currentTime = `${d}.${mont + 1} Time:${h}:${m}:${s}`;
     document.getElementById("dateTime").innerText = currentTime
     document.getElementById("dateTime").textContent = currentTime;
     document.getElementById("UserDisplay").innerText = username;
-   
+
     setTimeout(InfoDisplay, 1000);
 }
 
 InfoDisplay();
 
-
-
-
-
-
-
 //-----------------------------
-
-
-
-
-
-
-
-
+ColorTheme()
 function logIn() {
-    username = document.getElementById("username").value
-    credits = document.getElementById("credits").value
-    age = document.getElementById("age").value
-    playtime = document.getElementById("time").value
-    
-    let timetostop = timer()
-    document.getElementById("TimerDisplay").innerText ="casino closes at" + timetostop;
-    //timer(time)
-
-    let aged = +age
-    if(aged < 18){
-       alert("Du måste vara minst 18 år för att kunna registrera dig.")
-    }
-    else{ localStorage.setItem("age", age)}
-
-    if(username == ""){
-        alert("username is needed")
-    }
-    else { localStorage.setItem("username", username)}
-    
-    if(credits == "" || credits <= 0){
-        alert("You need to have at least one credit to play!")}
-        else {localStorage.setItem("credits", credits)}
-
-        console.log(`Username: ${username}, Credits: ${credits}, Age: ${age}`);
-        console.log(localStorage)
-        
+    getvalues();
+    timer()
+    localStorageFunction(aged);
     StealYourInfo()
-
-
-        
-
+    if (aged >= 18) {
+        hideLogin()
+    }
+    
 
 }
 
-    function timer(){
+function localStorageFunction(aged) {
+    if (aged < 18) {
+        alert("Du måste vara minst 18 år för att kunna registrera dig.");
+    }
+    else { localStorage.setItem("age", age); }
+
+    if (username == "") {
+        alert("username is needed");
+    }
+    else { localStorage.setItem("username", username); }
+
+    if (credits == "" || credits <= 0) {
+        alert("You need to have at least one credit to play!");
+    }
+    else { localStorage.setItem("credits", credits); }
+
+    console.log(`Username: ${username}, Credits: ${credits}, Age: ${age}`);
+    console.log(localStorage);
+}
+
+function getvalues() {
+    username = document.getElementById("username").value;
+    credits = document.getElementById("credits").value;
+    age = document.getElementById("age").value;
+    playtime = document.getElementById("time").value;
+}
+
+function timer() {
     let date = new Date();
     let h = date.getHours();
     let m = date.getMinutes();
     let s = date.getSeconds();
-    
-    aTime =+ playtime
+
+    aTime = + playtime
     m = m + aTime
-    
-   for(let i = 0; i = 1; i++)
-    if(m > 60){
-        h++;
-        m = m - 60;
-        i--;
-    }
-    else{
-        break;
-    }
-    
-     let playTimeStop = `${h}:${m}:${s}`
-    
+
+    for (let i = 0; i = 1; i++)
+        if (m > 60) {
+            h++;
+            m = m - 60;
+            i--;
+        }
+        else {
+            break;
+        }
+
+    let playTimeStop = `${h}:${m}:${s}`
+
     console.log(playTimeStop)
-    return playTimeStop;
-    
+    document.getElementById("TimerDisplay").innerText = "casino closes at" + playTimeStop;
+
+
 
 }
 
-function StealYourInfo(){
+function StealYourInfo() {
     console.log(navigator.platform) //platform
     console.log(navigator.userAgent) //browser 
     console.log(navigator.language) //språk
@@ -122,43 +114,81 @@ function StealYourInfo(){
     timesplayed()
 }
 
-function getlocation(){
-    navigator.geolocation.getCurrentPosition(function(position) {
-            
-            let latitude = position.coords.latitude
-            let longitude = position.coords.longitude
-    
-            console.log(`latitude ${latitude}`)
-            console.log(`longitude ${longitude}`)
-        },
-        function(error) {
+function getlocation() {
+    navigator.geolocation.getCurrentPosition(function (position) {
+
+        let latitude = position.coords.latitude
+        let longitude = position.coords.longitude
+
+        console.log(`latitude ${latitude}`)
+        console.log(`longitude ${longitude}`)
+    },
+        function (error) {
             console.log('Error Message = ', error.message)
         }
     )
 }
 
-function timesplayed(){
-    
+function timesplayed() {
+
     let j = +localStorage.timesplayed
-    let i = j 
-    if(localStorage.timesplayed >= 1){
+    let i = j
+    if (localStorage.timesplayed >= 1) {
         i++
         localStorage.setItem("timesplayed", i)
-        console.log(localStorage.timesplayed)
+        console.log("times played" + localStorage.timesplayed)
     }
-    else{
+    else {
         console.log("username is new")
         localStorage.setItem("timesplayed", 1)
     }
-    
+
 }
 
-/*
-- Plattform
-- webbläsare
-- språk
-- Resolution
-- windowSize
-- Longitude Latitude (Meddelande om användaren nekar geolocation)
-- Cookie/localstorage som trackar hur många gånger man har spelat och skriver ut det
-*/
+function hideLogin() {
+
+    const loginSection = document.getElementById("welcome")
+    loginSection.style.display = "none"
+
+}
+function saveTheme(){
+    const theme = document.getElementById("ColorTheme")
+    localStorage.setItem("colortheme", theme.value)
+}
+function ColorTheme(){
+    
+    
+    let colorvalue
+    let textvalue
+
+    if(localStorage.getItem("colortheme") == "light"){
+        console.log("light")
+         colorvalue = "#ffffff"
+         textvalue = "#000000"
+    }if(localStorage.getItem("colortheme") == "dark"){
+        console.log("dark")
+        colorvalue = "#283593"
+        textvalue = "#FFFFFF"
+    }if(localStorage.getItem("colortheme") == "gambler"){
+        console.log("huuuh")
+        colorvalue = colorGambler()
+        textvalue = colorGambler()
+    }
+
+    console.log(colorvalue + textvalue)
+
+    setColor(colorvalue, textvalue);
+}
+
+
+
+function setColor(colorvalue, textvalue) {
+    document.body.style.backgroundColor = colorvalue;
+    document.body.style.color = textvalue;
+}
+
+colorGambler()
+function colorGambler(){
+    let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    return "#"+randomColor
+}
